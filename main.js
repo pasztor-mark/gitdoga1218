10c1b16de5e611e38fe3b3751768d2994997bdc3
Petrik Diak <diak@petrik.hu>
2023-12-18 12:27:17 +0100

    Harmadik feladat - kilistázás

--- main.js
+++ main.js
@@ -3,6 +3,16 @@ function szamFelvetel() {
     var ertek = document.getElementById("szam").value
     if (ertek > 0 || ertek <= 0) {
         szamlista.push(ertek)
+        ujListaelem(ertek)
     }
-    console.log(szamlista)
+    
+}
+
+function ujListaelem(ertek) {
+    var text = document.getElementById("lista").innerHTML
+    if (szamlista.length > 1) {
+        text += ", "
+    }
+    text += ertek
+    document.getElementById("lista").innerHTML = text
 }
